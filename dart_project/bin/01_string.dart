186defd97f86e404ecd9ef2c4c015bff3e182d22
khanifzyen <khanif.zyen@gmail.com>
2024-03-04 08:22:09 +0700

    string

---
+++ dart_project/bin/01_string.dart
@@ -0,0 +1,4 @@
+void main() {
+  String data = "Hello Pemrograman Mobile";
+  print(data);
+}
--- dart_project/bin/dart_project.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_project/dart_project.dart' as dart_project;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_project.calculate()}!');
-}
