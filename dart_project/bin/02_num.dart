1249314ee5cc66057b79f9ce1d9128923faa32cd
MuhammadBayuNugroho <bayu27.ngrh@gmail.com>
2024-03-05 11:48:21 +0700

    number

---
+++ dart_project/bin/02_num.dart
@@ -0,0 +1,9 @@
+void main() {
+  num data = 20;
+  print(data);
+  // jika kita mencetak data maka hasilnya 20
+
+  num data2 = 22.2;
+  print(data2);
+  // jika kita mencetak data2 maka hasilnya 22.2
+}
