9c1d3335a490793f3504febf70043568cf1fcfba
MuhammadBayuNugroho <bayu27.ngrh@gmail.com>
2024-03-05 12:42:57 +0700

    operator aritmatika, relasional, logika

---
+++ dart_project/bin/12_operator.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Operator Aritmatika
+  int c = 6;
+  c = c + 6;
+  print(c); // Hasilnya 12
+
+  // Operator Relasional
+  String kota = 'Jepara';
+  if (kota == 'Jepara')
+    true;
+  else
+    false;
+  print(kota); // Hasilnya Jepara
+
+  // Operator Logika
+  var A = true;
+  var B = false;
+  var expr = A && B;
+  print(expr); // Hasilnya false
+}
